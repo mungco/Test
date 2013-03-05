36fa7f1be671fb505fc2595833fc2294689b4407
mungco <logarrow@sina.com>
2013-03-05 10:58:14 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc , char *args[])
+{
+    for(int i=0 ; i<3 ; i++)
+        cout<<i<<endl;
+    for(int i=0 ; i<3 ; i++)
+        cout<<i<<endl;
+    return 1;
+}
